b91c14048e0bd0282ba347edde5c5d324f1c9ceb
Aravind <142539478+Aravind1004venu@users.noreply.github.com>
2024-07-18 23:52:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "The programming process is a .............?",
+    option1: "System development process",
+    option2: "Coding process",
+    option3: "Testing process",
+    option4: "Problem solving process",
+    ans: 4,
+  },
+  {
+    question: "Who usually provides the problem definiton to a programmer ?",
+    option1: "The system analyst",
+    option2: "The program manager",
+    option3: "The project manager",
+    option4: "A senior programmer",
+    ans: 1,
+  },
+  {
+    question: "What is used for keeping track of the number of times something occurs in a program ?",
+    option1: "A loop",
+    option2: "A counter",
+    option3: "A decision construct",
+    option4: "None of the above",
+    ans: 2,
+  },
+  {
+    question: "Which command will stop an infinite loop ?",
+    option1: "Alt-C",
+    option2: "Shift-C",
+    option3: "Esc",
+    option4: "Ctrl-C",
+    ans: 4,
+  },
+  {
+    question: "Which of these is not an access modofier in Java ?",
+    option1: "Protected",
+    option2: "Public",
+    option3: "Private",
+    option4: "Package",
+    ans: 4,
+  },
+];
